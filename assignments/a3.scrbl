44636723b7ed6b9387524d61644d647e0683d753
hazel levine <hazel@knightsofthelambdacalcul.us>
2023-02-11 23:58:34 -0500

    a3 upd8s

--- assignments/a3.scrbl
+++ assignments/a3.scrbl
@@ -9,8 +9,8 @@
 @; 8y: olive, 2023-02-06
 @title[#:style (with-html5 manual-doc-style)]{Assignment 3: Normalization by evaluation}
 
-This assignment will be assigned on February 7, 2023, and will be due on February 14, 2023.
-Corrections will be accepted until February 28, 2023.
+This assignment will be assigned on February 7, 2023, and will be due on February @bold{17}, 2023.
+Corrections will be accepted until @bold{March 3}, 2023.
 
 The purpose of this assignment is to extend your work for A2, and normalize the core terms that
 come out of your elaborating type checker. This change requires a lot of mechanical work, and also
@@ -31,8 +31,8 @@ In the rest of this, we will be extending our normalizer to handle computation a
 for each of our types in A2. We start with our A2 code, and add the normalizer in the starter code,
 available @hyperlink["starter/nbe.rkt"]{here}.
 
-@bold{Exercise 1:} @italic{Products.} We add a computation rule to our system for products, but not a
-uniqueness rule. Note that we @italic{could} add a uniqueness rule, but we do not.
+@bold{Exercise 0:} @italic{Products (done in class).} We add a computation rule to our system for products,
+but not a uniqueness rule. Note that we @italic{could} add a uniqueness rule, but we do not.
 
 @$${
 \ir{(\times_{\beta_1})}
@@ -54,34 +54,46 @@ When encountering a neutral, push @tt{value-fst} and @tt{value-snd} onto the spi
 Then, extend @tt{reify} to account for it as well, modifying @tt{reify-form} and @tt{reify}. Make sure to
 preserve type information throughout your cuts.
 
+@bold{Exercise 1:} @italic{Motives.} Note that our @tt{Syntax} and @tt{Value} data definitions have changed
+slightly from those in class. In particular, @tt{syntax-+-elim} now has a @tt{motive} field, which is a
+@italic{type}. It represents the type we are eliminating into: for example, if the result of @tt{syntax-+-elim}
+is @${C} and you're eliminating an @${A + B}, then the motive argument is @${C} and the function types
+would be @${A \to C} and @${B \to C}.
+
+Don't change your @tt{ConcreteSyntax} data definition: instead, in the tactics handling @tt{+-elim} and
+@tt{⊥-elim}, add the motive as an argument during elaboration. So, if the tactic returns a @${C}, put that
+@${C} in the resultant syntax as well.
+
 @bold{Exercise 2:} @italic{Sums.} Here's the computation rules (again, no uniqueness) for sums:
 
 @$${
 \ir{(+_{\beta_1})}
    {\Gamma \vdash f : A \to C ~~~ \Gamma \vdash g : B \to C ~~~ \Gamma \vdash a : A}
-   {\Gamma \vdash \mathrm{+-elim}\ f\ g\ (\mathrm{inl}\ a) = f\ a : C}
+   {\Gamma \vdash \mathrm{+-elim}\ C\ (\mathrm{inl}\ a)\ f\ g = f\ a : C}
 ~~~
 \ir{(+_{\beta_2})}
    {\Gamma \vdash f : A \to C ~~~ \Gamma \vdash g : B \to C ~~~ \Gamma \vdash b : B}
-   {\Gamma \vdash \mathrm{+-elim}\ f\ g\ (\mathrm{inr}\ b) = g\ b : C}
+   {\Gamma \vdash \mathrm{+-elim}\ C\ (\mathrm{inr}\ b)\ f\ g = g\ b : C}
 }
 
-Again, extend @tt{evaluate} to handle @tt{syntax-inl} and @tt{syntax-inr}. Then, design a function
+Again, extend @tt{evaluate} and @tt{reify} to handle @tt{syntax-inl} and @tt{syntax-inr}. Then, design a function
 @tt{do-+-elim : Value Value Value -> Value} accounting for all relevant @tt{Value}s. When encountering a neutral,
 push @tt{value-+-elim} onto the spine.
+
+Note that @tt{value-+-elim} takes @italic{four} arguments: the types for @${f} and @${g} in the above rules,
+and @${f}, @${g} themselves. This is because in @tt{reify-form}, we need to  have the types for @${f} and
+@${g} to recursively call @tt{reify}, as we did for application.
 
 @bold{Exercise 3:} @italic{Bottom.} We have no computation or uniqueness rule for bottom. However, it still
 needs to be added to @tt{evaluate} and @tt{reify}.
 
-Design a function @tt{do-⊥-elim : Value Type -> Value} that performs bottom elimination.
+Design a function @tt{do-⊥-elim : Type Value -> Value} that performs bottom elimination. Note that since
+we have no concrete values of type @${⊥}, all your scrutineés will be cuts.
 
-Note that you will have to pass in a type for typed normalization by evaluation to carry on.
-This means our surface syntax now @italic{must} have annotations on @italic{every} ⊥-elim,
-thereby making @tt{(syntax-ann (syntax-⊥-elim x) tp)} the only possibility, and @tt{(syntax-⊥-elim x)} a syntax
-error.
+Again, @tt{value-⊥-elim} needs a field for the type information --- this time, not because we need to recur
+in @tt{reify-form}, but because our @tt{syntax-⊥-elim} @italic{also} needs that type information.
 
-If this seems convoluted, it's because it is. This will be fixed when we introduce dependent types, and the
-notion of a motive.
+Then, extend @tt{evaluate} and @tt{reify}.
 
 @bold{Exercise 4:} @italic{Tying the knot.} Modify @tt{run-chk} and @tt{run-syn} to produce a normal form of
 the elaborated syntax, rather than the elaborated syntax.
--- assignments/starter/nbe.rkt
+++ assignments/starter/nbe.rkt
@@ -1,6 +1,66 @@
 #lang racket
+(provide (all-defined-out))
 
-;; A2 here
+(module+ test
+  (require rackunit)
+
+  ; check-success intentionally removed
+  (define-syntax-rule (check-error exp)
+    (check-exn exn:fail? (thunk exp))))
+
+;; <A2 here: make sure that your Syntax data definition gets updated!>
+
+;; A [Maybe X] is one of:
+;; - X
+;; - #f
+
+;; A Syntax is one of:
+;; - (syntax-local Number)
+;; - (syntax-lam Symbol Syntax)
+;; - (syntax-app Syntax Syntax)
+;; - (syntax-hole [Maybe Syntax])
+;; - (syntax-cons Syntax Syntax)
+;; - (syntax-fst Syntax)
+;; - (syntax-snd Syntax)
+;; - (syntax-inl Syntax)
+;; - (syntax-inr Syntax)
+;; - (syntax-+-elim Type Syntax Syntax Syntax)
+;; - (syntax-⊥-elim Type Syntax)
+(struct syntax-local (n) #:transparent)
+(struct syntax-lam (var body) #:transparent)
+(struct syntax-app (rator rand) #:transparent)
+(struct syntax-ann (tm tp) #:transparent)
+(struct syntax-hole (contents) #:transparent)
+(struct syntax-cons (fst snd) #:transparent)
+(struct syntax-fst (pair) #:transparent)
+(struct syntax-snd (pair) #:transparent)
+(struct syntax-inl (value) #:transparent)
+(struct syntax-inr (value) #:transparent)
+(struct syntax-+-elim (motive scrut l-case r-case) #:transparent)
+(struct syntax-⊥-elim (motive scrut) #:transparent)
+
+;; A Type is one of:
+;; - (type-base Symbol)
+;; - (type-arrow Type Type)
+;; - (type-product Type Type)
+;; - (type-sum Type Type)
+;; - (type-bottom)
+(struct type-base (name) #:transparent)
+(struct type-arrow (domain range) #:transparent)
+(struct type-product (a b) #:transparent)
+(struct type-sum (a b) #:transparent)
+(struct type-bottom () #:transparent)
+
+;; parse : Sexp -> Type
+;; Parses an infix S-expression into a type.
+(define (parse s)
+  (match s
+    [`(,assumption → ,consequent) (type-arrow (parse assumption) (parse consequent))]
+    [`(,l × ,r) (type-product (parse l) (parse r))]
+    [`(,l + ,r) (type-sum (parse l) (parse r))]
+    [`(¬ ,v) (type-arrow (parse v) (type-bottom))]
+    ['⊥ (type-bottom)]
+    [(? symbol?) (type-base s)]))
 
 ;; A Closure is a (closure Syntax Environment)
 (struct closure (term env) #:transparent)
@@ -27,34 +87,32 @@
 ;; - (value-cons Value Value)
 ;; - (value-inl Value)
 ;; - (value-inr Value)
-;; - (value-ann Value Type)
 (struct value-lam (name clos) #:transparent)
 (struct value-cons (first second) #:transparent)
 (struct value-inl (left) #:transparent)
 (struct value-inr (right) #:transparent)
-(struct value-ann (val tp) #:transparent)
 
-;; A Cut is a (value-cut Type Head [ListOf Form])
-(struct value-cut (tp head spine) #:transparent)
+;; A Cut is a (cut Type Head [ListOf Form])
+(struct cut (tp head spine) #:transparent)
 
 ;; A Form is one of:
-;; - (value-app Value)
-;; - (value-fst)
-;; - (value-snd)
-;; - (value-+-elim Value Value)
-;; - (value-⊥-elim)
-(struct value-app (rand) #:transparent)
-(struct value-fst () #:transparent)
-(struct value-snd () #:transparent)
-(struct value-+-elim (l-case r-case) #:transparent)
-(struct value-⊥-elim () #:transparent)
+;; - (form-app Type Value)
+;; - (form-fst)
+;; - (form-snd)
+;; - (form-+-elim Type Type Value Value)
+;; - (form-⊥-elim Type)
+(struct form-app (tp rand) #:transparent)
+(struct form-fst () #:transparent)
+(struct form-snd () #:transparent)
+(struct form-+-elim (l-case-tp r-case-tp l-case r-case) #:transparent)
+(struct form-⊥-elim (motive) #:transparent)
 
 ;; A Head is one of:
-;; - (value-local Number)
-;; *interpretation*: a value-local is a DeBruijn level that when quoted
+;; - (head-local Number)
+;; *interpretation*: a head-local is a DeBruijn level that when quoted
 ;;                   gets read into a DeBruijn index to avoid having to
 ;;                   do shifting math in reify
-(struct value-local (idx) #:transparent)
+(struct head-local (idx) #:transparent)
 
 ;; evaluate : Syntax Environment -> Value
 ;; Evaluates the expression to a normal form.
@@ -64,15 +122,82 @@
     [(syntax-local n) (apply-env env n)]
     [(syntax-lam value body) (value-lam value (closure body env))]
     [(syntax-app rator rand) (do-app (evaluate rator env)
-                                     (evaluate rand env))]))
+                                     (evaluate rand env))]
+    [(syntax-cons first second) (value-cons (evaluate first env)
+                                            (evaluate second env))]
+    [(syntax-fst p) (do-fst (evaluate p env))]
+    [(syntax-snd p) (do-snd (evaluate p env))]))
+
+(module+ test
+  (check-equal?
+   (evaluate (syntax-fst (syntax-cons (syntax-lam 'x (syntax-local 0))
+                                      (syntax-lam 'x (syntax-lam 'y (syntax-local 0)))))
+             (empty-env))
+   (evaluate (syntax-lam 'x (syntax-local 0))
+             (empty-env))) 
+
+  (check-equal?
+   (evaluate (syntax-app (syntax-lam 'x (syntax-lam 'y (syntax-local 1)))
+                         (syntax-lam 'x (syntax-local 0)))
+             (empty-env))
+   (do-app (evaluate (syntax-lam 'x (syntax-lam 'y (syntax-local 1))) (empty-env))
+           (evaluate (syntax-lam 'x (syntax-local 0)) (empty-env))))
+
+  (check-equal?
+   (evaluate (syntax-lam 'y (syntax-local 1))
+             (extend-env (empty-env) (value-lam 'x (closure (syntax-local 0) (empty-env)))))
+   (value-lam 'y (closure (syntax-local 1)
+                          (extend-env (empty-env) (value-lam 'x (closure (syntax-local 0) (empty-env)))))))
+
+  (check-equal?
+   (evaluate (syntax-local 1)
+             (extend-env
+              (extend-env (empty-env)
+                          (value-lam 'x (closure (syntax-local 0) (empty-env))))
+              (cut (parse 'A) (head-local 0) '())))
+   (value-lam 'x (closure (syntax-local 0) (empty-env))))
+
+  ;; ---
+
+  (check-equal?
+   (evaluate (syntax-lam 'x (syntax-lam 'y (syntax-local 1))) (empty-env))
+   (value-lam 'x (closure (syntax-lam 'y (syntax-local 1)) (empty-env))))
+  (check-equal?
+   (evaluate (syntax-lam 'x (syntax-local 0)) (empty-env))
+   (value-lam 'x (closure (syntax-local 0) (empty-env)))))
 
 ;; do-app : Value Value -> Value
 ;; Applies the rator to the rand.
 (define (do-app rator rand)
   (match rator
     [(value-lam _ closure) (apply-closure closure rand)]
-    [(value-cut (type-arrow A B) head spine)
-     (value-cut B head (cons (value-app (value-ann rand A)) spine))]))
+    [(cut (type-arrow A B) head spine)
+     (cut B head (cons (form-app A rand) spine))]))
+
+(module+ test
+  (check-equal?
+   (do-app (value-lam 'x (closure (syntax-lam 'y (syntax-local 1)) (empty-env)))
+           (value-lam 'x (closure (syntax-local 0) (empty-env))))
+   (apply-closure (closure (syntax-lam 'y (syntax-local 1)) (empty-env))
+                  (value-lam 'x (closure (syntax-local 0) (empty-env)))))
+
+  (check-equal?
+   (do-app
+    (value-lam 'y (closure (syntax-local 1)
+                           (extend-env (empty-env)
+                                       (value-lam 'x (closure (syntax-local 0) (empty-env))))))
+    (cut (parse 'A) (head-local 0) '()))
+   (apply-closure (closure (syntax-local 1)
+                           (extend-env (empty-env)
+                                       (value-lam 'x (closure (syntax-local 0) (empty-env)))))
+                  (cut (parse 'A) (head-local 0) '())))
+
+  (check-equal?
+   (do-app
+    (value-lam 'x (closure (syntax-local 0) (empty-env)))
+    (cut (parse 'B) (head-local 1) '()))
+   (apply-closure (closure (syntax-local 0) (empty-env))
+                  (cut (parse 'B) (head-local 1) '()))))
 
 ;; apply-closure : Closure Value -> Value
 ;; Applies the closure to the value.
@@ -80,40 +205,164 @@
   (match-define (closure tm env) clo)
   (evaluate tm (extend-env env rand)))
 
+(module+ test
+  (check-equal?
+   (apply-closure (closure (syntax-lam 'y (syntax-local 1)) (empty-env))
+                  (value-lam 'x (closure (syntax-local 0) (empty-env))))
+   (evaluate (syntax-lam 'y (syntax-local 1))
+             (extend-env (empty-env) (value-lam 'x (closure (syntax-local 0) (empty-env))))))
+
+  (check-equal?
+   (apply-closure (closure (syntax-local 1)
+                           (extend-env (empty-env)
+                                       (value-lam 'x (closure (syntax-local 0) (empty-env)))))
+                  (cut (parse 'A) (head-local 0) '()))
+   (evaluate (syntax-local 1)
+             (extend-env
+              (extend-env (empty-env)
+                          (value-lam 'x (closure (syntax-local 0) (empty-env))))
+              (cut (parse 'A) (head-local 0) '()))))
+
+  (check-equal?
+   (apply-closure (closure (syntax-local 0) (empty-env))
+                  (cut (parse 'B) (head-local 1) '()))
+   (cut (parse 'B) (head-local 1) '())))
+
+;; do-fst : Value -> Value
+;; Takes the first of the given value.
+(define (do-fst p)
+  (match p
+    [(value-cons a b) a]
+    [(cut (type-product A B) head spine)
+     (cut A head (cons (form-fst) spine))]))
+
+(module+ test
+  (check-equal?
+   (do-fst (value-cons (cut (parse 'A) (head-local 0) '())
+                       (cut (parse 'A) (head-local 1) '())))
+   (cut (parse 'A) (head-local 0) '()))
+  (check-equal?
+   (do-fst (cut (parse '(A × B)) (head-local 0) '()))
+   (cut (parse 'A) (head-local 0) (cons (form-fst) '()))))
+
+;; do-snd : Value -> Value
+;; Takes the second of the given value.
+(define (do-snd p)
+  (match p
+    [(value-cons a b) b]
+    [(cut (type-product A B) head spine)
+     (cut B head (cons (form-snd) spine))]))
+
 ;; reify : Number Value Type -> Syntax
 ;; Turns a value under the given environment size into a syntax.
 (define (reify size val tp)
-  (match tp
-    [(type-base _)
-     (match val
-       [(value-cut _ head spine)
-        (reify-spine size (reify-head size head) spine)])]
+  (match tp    
     [(type-arrow A B)
      (syntax-lam
       (match val
         [(value-lam name _) name]
         [_ (gensym)])
       (reify (add1 size)
-             (do-app val (value-cut A (value-local size) '()))
-             B))]))
+             (do-app val (cut A (head-local size) '()))
+             B))]
+    [(type-product A B)
+     (match val
+       [(value-cons fst snd) (syntax-cons (reify size fst A)
+                                          (reify size snd B))]
+       [(cut _ _ _) (reify-cut size val)])]  
+    ;; only thing that can be a base type is a variable, represented by a cut
+    ;; so don't even bother matching
+    [(type-base _) (reify-cut size val)]))
+
+;; reify-cut : Number Cut -> Syntax
+;; Quotes a cut, by quoting its head and adding back all its eliminators.
+(define (reify-cut size c)
+  (match-define (cut _ head spine) c)
+  (reify-spine size (reify-head size head) spine))
+
+(module+ test
+  (check-equal?
+   (reify 0
+          (value-cons (value-lam 'x (closure (syntax-local 0) (empty-env)))
+                      (value-lam 'y (closure (syntax-local 0) (empty-env))))
+          (parse '((A → A) × (B → B))))
+   (syntax-cons (reify 0 (value-lam 'x (closure (syntax-local 0) (empty-env))) (parse '(A → A)))
+                (reify 0 (value-lam 'y (closure (syntax-local 0) (empty-env))) (parse '(B → B)))))
+
+  (check-equal?
+   (reify 1
+          (cut (parse '(A × B)) (head-local 0) '())
+          (parse '(A × B)))
+   (syntax-local 0))
+
+  (check-equal?
+   (reify
+    0
+    (value-lam 'y (closure (syntax-local 1)
+                           (extend-env (empty-env)
+                                       (value-lam 'x (closure (syntax-local 0) (empty-env))))))
+    (parse '(A → (B → B))))
+   (syntax-lam
+    'y
+    (reify 1
+           (do-app
+            (value-lam 'y (closure (syntax-local 1)
+                                   (extend-env (empty-env)
+                                               (value-lam 'x (closure (syntax-local 0) (empty-env))))))
+            (cut (parse 'A) (head-local 0) '()))
+           (parse '(B → B)))))
+
+  (check-equal?
+   (reify 1
+          (value-lam 'x (closure (syntax-local 0) (empty-env)))
+          (parse '(B → B)))
+   (syntax-lam
+    'x
+    (reify 2
+           (do-app
+            (value-lam 'x (closure (syntax-local 0) (empty-env)))
+            (cut (parse 'B) (head-local 1) '()))
+           (parse 'B))))
+
+  (check-equal?
+   (reify 2
+          (cut (parse 'B) (head-local 1) '())
+          (parse 'B))
+   (syntax-local 0)))
 
 ;; reify-head : Number Head -> Syntax
 ;; Turns a head into a syntax.
 (define (reify-head size head)
   (match head
-    [(value-local lvl) (syntax-local (- size lvl 1))]))
+    [(head-local lvl) (syntax-local (- size lvl 1))]))
+
+(module+ test
+  (check-equal? (reify-head 2 (head-local 1))
+                (syntax-local 0)))
 
 ;; reify-spine : Number Syntax [ListOf Form] -> Syntax
 (define (reify-spine size exp spine)
   (match spine
     ['() exp]
     [(cons form rst)
-     (reify-spine size (reify-form size exp form) rst)]))
+     (reify-form size (reify-spine size exp rst) form)]))
+
+(module+ test
+  (check-equal?
+   (reify-spine 2 (syntax-local 0) '())
+   (syntax-local 0)))
 
 ;; reify-form : Number Syntax Form -> Syntax
 (define (reify-form size exp form)
   (match form
-    [(value-app (value-ann rand A)) (syntax-app exp (reify size rand A))]))
+    [(form-app A rand) (syntax-app exp (reify size rand A))]
+    [(form-fst) (syntax-fst exp)]
+    [(form-snd) (syntax-snd exp)]))
+
+(module+ test
+  (check-equal?
+   (reify-form 1 (syntax-local 0) (form-fst))
+   (syntax-fst (syntax-local 0))))
 
 ;; normalize : Syntax Type -> Syntax
 ;; Produces the normal form of the given syntax.
